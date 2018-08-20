d7355f8bc7bfac44a7a8710f1633eba1665eaabb
William del Rosario <williamdelrosario@yahoo.com>
2018-08-20 16:14:09 +0200

    create and require router class

---
+++ core/Router.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: williamdelrosario
+ * Date: 8/20/18
+ * Time: 4:08 PM
+ */
+
+/**
+ * Class Router
+ */
+class Router
+{
+}
--- public_html/index.php
+++ public_html/index.php
@@ -5,5 +5,19 @@
  * Date: 8/20/18
  * Time: 3:52 PM
  */
+/**
+ * Front Controller
+ *
+ * PHP 7.2.1
+ */
 
-echo "hello";
+//echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+require '../core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);
