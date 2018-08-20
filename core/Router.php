cafcd997cfae3656027d51db9db295487b2d7cab
William del Rosario <williamdelrosario@yahoo.com>
2018-08-20 19:47:22 +0200

    get controller action form url with a variable structure

--- core/Router.php
+++ core/Router.php
@@ -30,8 +30,17 @@ class Router
 	 *
 	 * @return void
 	 */
-	public function add($route, $params)
+	public function add($route, $params = [])
 	{
+		// Convert the route to a regular expression: escape forward slashes
+		$route = preg_replace('/\//', '\\/', $route);
+
+		// Convert variables e.g. {controller}
+		$route = preg_replace('/\{([a-z])\}/', '(?P</1>[a-z]+)', $route);
+
+		// Add start and end delimiters, and case insensitive flag
+		$route = '/^' . $route . '$/i';
+
 		$this->routes[$route] = $params;
 	}
 
@@ -55,23 +64,25 @@ class Router
 	 */
 	public function match($url)
 	{
-		// Match the fixed URL format /controller/action
-		$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-
-		if (preg_match($reg_exp, $url, $matches))
+//		// Match the fixed URL format /controller/action
+//		$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+		foreach ($this->routes as $route => $params)
 		{
-			// Get named captured group values
-			$params = [];
-			foreach ($matches as $key => $match)
+			if (preg_match($route, $url, $matches))
 			{
-				if (is_string($key))
+				// Get named captured group values
+//				$params = [];
+				foreach ($matches as $key => $match)
 				{
-					$params[$key] = $match;
+					if (is_string($key))
+					{
+						$params[$key] = $match;
+					}
 				}
-			}
 
-			$this->params = $params;
-			return true;
+				$this->params = $params;
+				return true;
+			}
 		}
 		return false;
 	}
--- public_html/index.php
+++ public_html/index.php
@@ -16,21 +16,29 @@
  */
 require '../core/Router.php';
 $router = new Router();
-//echo get_class($router);
+
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
 
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
+//// Match the requested route
+//$url = $_SERVER['QUERY_STRING'];
 
-if($router->match($url))
-{
-	echo '<pre>';
-	var_dump($router->getParams());
-	echo '</pre>';
-} else
-{
-	echo "No route found for URL '$url'";
-}
+//if($router->match($url))
+//{
+//	echo '<pre>';
+//	var_dump($router->getParams());
+//	echo '</pre>';
+//} else
+//{
+//	echo "No route found for URL '$url'";
+//}
+
+// Display routing table
+echo '<pre>';
+//	var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '<pre>';
