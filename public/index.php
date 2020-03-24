77aadd75461460b7c4ab2155993b8458ed93a182
william <william.del.rosario@iceshop.nl>
2020-03-24 16:24:48 +0100

    cleanup

--- public/index.php
+++ public/index.php
@@ -34,11 +34,12 @@ set_exception_handler('Core\Error::exceptionHandler');
 /**
  * Routing
  */
+// TODO Make this dynamic
 $router = new Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-//$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('posts/index', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}', ['namespace' => "Admin"]);
